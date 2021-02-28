925058ed747cc71f077b2af4bd50b2134e4dda14
CaglarGUL <caglar.gul@profen.com>
2021-02-28 16:13:36 +0300

    Usingler güncellendi.

--- MonitoringApp/WeatherService/Worker.cs
+++ MonitoringApp/WeatherService/Worker.cs
@@ -10,7 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RestSharp;
-using WeatherService.Model;
+using WeatherService.Models;
 
 namespace WeatherService
 {
